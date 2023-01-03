f1dba7d8a35c257905d4fca23119206c6a4e1865
Dominik Telka <dominiktelka@gmail.com>
2023-01-03 19:10:30 +0100

    destructed body from request and set type for it

--- routers/child.ts
+++ routers/child.ts
@@ -2,7 +2,12 @@ import {Router} from "express";
 import {ChildRecord} from "../records/child.record";
 import {GiftRecord} from "../records/gift.record";
 import {ValidationError} from "../utils/error";
-import {ListChildrenRes} from "../types/child/child";
+import {
+    CreateChildReq,
+    ListChildrenRes,
+    SetGiftForChildReq
+} from "../types/child/child";
+
 
 
 export const childRouter = Router();
@@ -10,25 +15,30 @@ export const childRouter = Router();
 childRouter
     .get('/', async (req,res)=>{
         const childrenList = await ChildRecord.listAll()  // over here we are taking all kids from function created inside Class ChildRecord. Later we need to create layout in list.hbs
-        const giftsList = await GiftRecord.listAll();
+        const giftList = await GiftRecord.listAll();
         res.json({
             childrenList,
-            giftsList,
-        } as unknown as ListChildrenRes);
+            giftList,
+        } as  ListChildrenRes);
     })
     .post('/', async (req,res)=>{
-        const newChild = new ChildRecord(req.body); // over here we are taking data like name from our website
+        const newChild = new ChildRecord(req.body as CreateChildReq); // over here we are taking data like name from our website
         await newChild.insert();
 
-        res.redirect('/child');
+        res.json(newChild);
     })
     .patch('/gift/:childId', async (req,res)=>{
-    const child = await ChildRecord.getOne(req.params.childId)
+
+        const {body}:{
+            body: SetGiftForChildReq
+        } = req;
+
+        const child = await ChildRecord.getOne(req.params.childId)
         if(child === null){
             throw new ValidationError('Nie znaleziono dziecka z podanym ID')
         }
 
-        const gift = req.body.giftid === '' ? null : await GiftRecord.getOne(req.body.giftid)
+        const gift = body.giftId === '' ? null : await GiftRecord.getOne(body.giftId)
         if(gift){
             if(gift.count <= await gift.countGivenGifts()){
                 throw new ValidationError('Tego prezentu jest za mało')
@@ -38,7 +48,7 @@ childRouter
         child.giftId = gift?.id ?? null;
         await child.update()
 
-        res.redirect('/child')
+        res.json(child)
 
     })
 
